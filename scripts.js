f1cef5c94646a54364b206d6dfe495c77edf67d0
Bar Vilek <humanbar@gmail.com>
2017-04-28 17:55:22 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
+
+
 function toggleNav() {
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("nav-bar-active");
+	
+	var content = document.getElementsByClassName("content");
+	console.log(content.length);
+	
+	var i;
+	for (i = 0; i < content.length; i++) {
+    	content[i].classList.toggle("content-nav-active");
+	}
+
 }
+
+
+var elem = document.querySelector('.all-items');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.item',
+  columnWidth: 200
+});
