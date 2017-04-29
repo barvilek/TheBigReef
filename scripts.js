b5efa23701bdc9781c8cbdde7807f3d11e319218
Bar Vilek <humanbar@gmail.com>
2017-04-29 14:25:54 +0300

    Add files via upload
    improved JavaScript & mobile meta tag match

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Masonry stuff
 var elem = document.querySelector('.all-items');
 var msnry = new Masonry( elem, {
   // options
@@ -11,19 +12,27 @@ imagesLoaded( '.all-items' ).on( 'progress', function() {
 });
 
 
+// Toggle nav-bar
+var navBtn = document.querySelector("#nav-btn");
 
-function toggleNav() {
-    var navbar = document.getElementById("navbar");
+navBtn.addEventListener("click", function () {
+	var navbar = document.getElementById("navbar");
     navbar.classList.toggle("nav-bar-active");
 
 	var content = document.getElementById("content");
 	content.classList.toggle("content-nav-active");
 	
 	msnry.layout();
-}
+});
 
-function removeItem(removeMe) {
-	msnry.remove(removeMe);
-	msnry.layout();
+// Remove an item
+var deleteBtns = document.querySelectorAll(".delete");
+
+for (var i=0; i<deleteBtns.length; i++) {
+	deleteBtns[i].addEventListener("click", function () {
+		var removeMe = this.parentNode.parentNode;
+		msnry.remove(removeMe);
+		msnry.layout();
+	});
 }
 
