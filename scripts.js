566a53aba5cbeadd491370a802c0101c24bc7817
Bar Vilek <humanbar@gmail.com>
2017-04-29 16:29:28 +0300

    Add files via upload
    finished

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 // Masonry stuff
 var elem = document.querySelector(".all-items");
-var msnry = new Masonry( elem, {
-  // options
+var msnry = new Masonry(elem, {
   itemSelector: ".item",
   horizontalOrder: true
 });
 
-imagesLoaded( ".all-items" ).on( "progress", function() {
-  // layout Masonry after each image loads
+imagesLoaded(".all-items").on("progress", function () {
   msnry.layout();
 });
 
@@ -16,23 +14,23 @@ imagesLoaded( ".all-items" ).on( "progress", function() {
 var navBtn = document.querySelector("#nav-btn");
 
 navBtn.addEventListener("click", function () {
-	var navbar = document.getElementById("navbar");
+    var navbar = document.getElementById("navbar");
     navbar.classList.toggle("nav-bar-active");
 
-	var content = document.getElementById("content");
-	content.classList.toggle("content-nav-active");
-	
-	msnry.layout();
+    var content = document.getElementById("content");
+    content.classList.toggle("content-nav-active");
+    
+    msnry.layout();
 });
 
 // Remove an item
 var deleteBtns = document.querySelectorAll(".delete");
 
-for (var i=0; i<deleteBtns.length; i++) {
-	deleteBtns[i].addEventListener("click", function () {
-		var removeMe = this.parentNode.parentNode;
-		msnry.remove(removeMe);
-		msnry.layout();
-	});
+for (var i=0; i < deleteBtns.length; i++) {
+    deleteBtns[i].addEventListener("click", function () {
+        var removeMe = this.parentNode.parentNode;
+        msnry.remove(removeMe);
+        msnry.layout();
+    });
 }
 
